29e7eddea0ef8dc62dd941adda252484d358ba70
Yurbus <frankins636@gmail.com>
2025-07-10 12:22:02 +0300

    errv

---
+++ telegram.php
@@ -0,0 +1,31 @@
+<?php
+// Настройки
+$token = "7751416363:AAEdMsqj2MjAcEuVtam9aUwad8SH0FxolsU"; // Заміни на свій токен
+$chat_id = "7751416363"; // Заміни на свій chat_id
+
+// Отримання даних з форми
+$name = $_POST['user_name'] ?? 'Не вказано';
+$phone = $_POST['user_phone'] ?? 'Не вказано';
+$telegram = $_POST['user_telegram'] ?? 'Не вказано';
+$instagram = $_POST['user_instagram'] ?? 'Не вказано';
+$box = $_POST['user_box'] ?? 'Не вказано';
+
+// Формування повідомлення
+$message = "📝 <b>Нове замовлення з сайту</b>\n";
+$message .= "👤 Ім'я: <b>$name</b>\n";
+$message .= "📞 Телефон: <b>$phone</b>\n";
+$message .= "💬 Telegram: <b>$telegram</b>\n";
+$message .= "📸 Instagram: <b>$instagram</b>\n";
+$message .= "📦 Пакет: <b>$box</b>";
+
+// Відправка до Telegram
+$url = "https://api.telegram.org/bot$token/sendMessage";
+
+$response = file_get_contents($url . "?" . http_build_query([
+    'chat_id' => $chat_id,
+    'text' => $message,
+    'parse_mode' => 'HTML'
+]));
+
+echo "OK";
+?>
